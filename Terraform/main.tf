ac70dcb7936e6499f889ee186cdbafbe27ef32e7
Venkatesan <vetrivenki@gmail.com>
2023-01-14 13:19:37 -0600

    First Terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,3 +1,10 @@
-output "name" {
-    value = "Hi, This is Venkat"
+provider "helm" {
+  kubernetes {
+	config_context_cluster   = "minikube"
+	
+  }
+}
+resource "helm_release" "example" {
+  name  = "redis"
+  chart = "https://charts.bitnami.com/bitnami/redis-10.7.16.tgz"
 }
